dfff7304b743bef64c535136b90564e814a10c26
djndl1 <djndl1@gmail.com>
2026-01-24 01:35:48 +0800

    vimrc: add initial LLM completion support

---
+++ .config/nvim/lua/llmrc.lua
@@ -0,0 +1,25 @@
+require('minuet').setup {
+    provider = 'openai_compatible',
+    n_completions = 1, -- recommend for local model for resource saving
+    -- I recommend beginning with a small context window size and incrementally
+    -- expanding it, depending on your local computing power. A context window
+    -- of 512, serves as an good starting point to estimate your computing
+    -- power. Once you have a reliable estimate of your local computing power,
+    -- you should adjust the context window to a larger value.
+    context_window = 256,
+    provider_options = {
+        openai_compatible = {
+            -- For Windows users, TERM may not be present in environment variables.
+            -- Consider using APPDATA instead.
+            api_key = 'MYQWEN_API_KEY',
+            name = 'Qwen',
+            end_point = 'https://dashscope.aliyuncs.com/compatible-mode/v1/chat/completions',
+            model = 'qwen3-coder-flash',
+            optional = {
+                max_tokens = 50,
+                top_p = 0.9,
+            },
+        },
+    },
+}
+
